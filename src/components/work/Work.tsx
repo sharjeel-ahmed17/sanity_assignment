0dee08bdfa690b4e751b3fc10c18c3413a8d5f97
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-12-21 05:59:13 +0500

    added

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,6 +1,7 @@
 // import {} from 'lucide-react'
 
 import { client } from "@/sanity/lib/client"
+import { urlFor } from "@/sanity/lib/image"
 import Image from "next/image"
 const getData = async () => {
     const res = await client.fetch(`*[_type == "blogs"]{
@@ -26,10 +27,17 @@ const Work = async() => {
         <div className="py-20 w-[80%]">
             <h2 className="text-[22px] ladding-[32.31px] font-medium mb-3">Featured works</h2>
             <div className="grid gird-col-1 md:grid-cols-[30%_auto]">
+                {/* <WorkCard />
                 <WorkCard />
                 <WorkCard />
-                <WorkCard />
-                <WorkCard />
+                <WorkCard /> */}
+
+
+{data.map((product: any, index: number)=> (
+    <div key={index} >
+    <WorkCard products={product}/>
+    </div>
+  ))}
             </div>
         </div>
     )
@@ -38,7 +46,7 @@ const Work = async() => {
 export default Work
 
 
-function WorkCard() {
+function WorkCard({products}: any) {
     return (
         <>
 
@@ -47,8 +55,8 @@ function WorkCard() {
                 
                 </div>
             <div>
-                <h3 className="font-bold text-[30px] leading-[44.06px] capitalize mb-3">designning dashboards</h3>
-                <p className="mb-3 capitalize"><span className="bg-red-500 px-4 py-2 text-white rounded-full mr-10 text-[18px] ladding-[26.44]">2020</span> <span className="text-[20px]"> dashboard</span> </p>
+                <h3 className="font-bold text-[30px] leading-[44.06px] capitalize mb-3">{products.title}</h3>
+                <p className="mb-3 capitalize"><span className="bg-red-500 px-4 py-2 text-white rounded-full mr-10 text-[18px] ladding-[26.44]">{products.year}</span> <span className="text-[20px]"> {products.category}</span> </p>
                 <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
             </div>
         </>
